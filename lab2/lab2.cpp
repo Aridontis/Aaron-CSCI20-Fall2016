87e7a85522077256a8f238a449035f71945251df
Aaron George <ageorge003@student.butte.edu>
2016-09-06 18:39:59 +0000

    Lab 4 submission

---
+++ lab2/lab2.cpp
@@ -0,0 +1,13 @@
+//Created By: Aaron George
+//Created On: 8/30/2016
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "  ()()  " << endl;
+    cout << "  (-.-) " << endl;
+    cout << "  o_(')(') " << endl;
+    
+    return 0;
+}
---
+++ lab3/lab3.cpp
@@ -0,0 +1,31 @@
+//Created By: Aaron George
+//Created On: 8/30/2016
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    string weapname;
+    string badname;
+    int companions;
+    string bodypart;
+    
+    //user input
+    cout << "Please enter a name:" ;
+    getline(cin, name);
+    cout << endl;
+    
+    cout << "Please give the name of your weapon:" ;
+    getline(cin, weapname);
+    cout << endl;
+    
+    cout << "Please enter your enemy's name:";
+    getline(cin, badname);
+    cout << endl;
+    
+
+}
