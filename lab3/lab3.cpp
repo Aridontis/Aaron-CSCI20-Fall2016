aa403845c6d96a7721c873e95bd73b55c9cce2f1
Aaron George <ageorge003@student.butte.edu>
2016-09-13 21:18:52 +0000

    Lab 3 submission

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -13,6 +13,7 @@ int main()
     string badname;
     int companions;
     string bodypart;
+    string reward;
     
     //user input
     cout << "Please enter a name:" ;
@@ -27,5 +28,20 @@ int main()
     getline(cin, badname);
     cout << endl;
     
+    cout << "Please enter a bodypart:";
+    getline(cin, bodypart);
+    cout << endl;
+
+cout << "Please enter a plural noun:";
+    getline(cin, reward);
+    cout << endl;
+    
+    cout << "Please enter a number:";
+    cin >> companions;
+    cout << endl;
+    
+cout << name << " is a hero. Getting the great weapon " << weapname << "," << name << " set out on a quest to defeat the evil " << badname << "."; 
+cout << " With " << companions << " companions following him, he enter the hall of doom, where he defeated the evil " << badname << ", by sticking " << weapname << " up into his " << bodypart << ".";
+cout << "Thus vanquished," << name << " returned home where people gave him many " << reward <<".";
 
 }
